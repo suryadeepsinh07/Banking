70d40b56c47e8343cdb743b0f017a1dcf4f95e1b
suryadeepsinh07 <zalasuryadeepsinh136@gmail.com>
2024-09-25 15:02:32 -0400

    sign-in page

---
+++ my-app/components/ui/AuthForm.tsx
@@ -0,0 +1,111 @@
+'use client'
+import Link from 'next/link'
+import Image from 'next/image'
+import React, { useState } from 'react'
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import CustomInput from './CustomInput'
+import { authFormSchema } from '@/lib/utils'
+import { Loader } from 'lucide-react'
+
+
+const AuthForm = ({ type }: { type: string }) => {
+    const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof authFormSchema>>({
+        resolver: zodResolver(authFormSchema),
+        defaultValues: {
+            email: "",
+            password: ''
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof authFormSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        setIsLoading(true)
+        console.log(values)
+        setIsLoading(false)
+    }
+    return (
+        <div className='auth-form'>
+            <header className='flex flex-col gap-5 md:gap-8'>
+                <Link href="/"
+                    className=" cursor-pointer items-center gap-1 flex px-4">
+                    <Image
+                        src="/icons/logo.svg"
+                        width={34}
+                        height={34}
+                        alt='Horizon Logo'
+                    />
+                    <h1 className='text-26 font-ibm-plex-serif font-bold text-black-1'>Horizon</h1>
+                </Link>
+                <div className='flex flex-col gap-1 md:gap-3'>
+                    <h1 className='text-24 lg:text-36 fontsemi text-gray-900 '>
+                        {user ? 'Link Account' : type === 'sign-in' ? 'Sign In' : 'Sign Up'}
+                    </h1>
+                    <p className='text-16 font-normal text-gray-600'>
+                        {user ?
+                            'Link your account to get started' : 'Please enter your details'}
+                    </p>
+                </div>
+            </header>
+            {user ? (
+                <div className='flex flex-col gap-4'>
+                    {/* PlaidLink */}
+                </div>
+            ) :
+                <>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                            <CustomInput
+                                control={form.control}
+                                name="email"
+                                label="Email"
+                                placeholder="Enter your email"
+                            />
+                            <CustomInput
+                                control={form.control}
+                                name="password"
+                                label="Password"
+                                placeholder="Enter your password"
+                            />
+                            <div className='flex flex-col gap-4'>
+                                <Button type="submit" disabled={isLoading} className='form-btn'>
+                                    {isLoading ? (
+                                        <>
+                                            <Loader size={20}
+                                                className='animate-spin' />&nbsp;
+                                            Loading...
+                                        </>
+                                    ) : type === 'sign-in' ? "Sign In" : "Sign Up"}
+                                </Button>
+                            </div>
+                        </form>
+                    </Form>
+                    <footer className='flex justify-center gap-1'>
+                        <p className='text-14 font-normal text-gray-400'>{type === 'sign-in' ? "Don't have an account?" : "Already have an account?"}</p>
+                        <Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className='form-link'>
+                            {type === 'sign-in' ? 'Sign Up' : 'Sign In'}
+                        </Link>
+                    </footer>
+                </>}
+        </div>
+    )
+}
+
+export default AuthForm
